50aab3d9c15671f632804f9062bcdda742aea1e0
dongxuemin666 <dongxuemin1995@163.com>
2021-04-14 10:00:06 +0800

    revise

--- SSN/SSN.sh
+++ SSN/SSN.sh
@@ -8,8 +8,6 @@ out=`sed '/^out=/!d;s/.*=//' conf_ssn.txt`
 
 
 
-python interaction.py -i $normal_gene_matrix
+python interaction.py -i $normal_gene_matrix -o $out
 
-python compute_SSN.py -background="all_gene_pairs.txt" -pvalue=$pvalue -ref="$normal_gene_matrix" -sample="$case_gene_matrix" -out="$out"
-
-rm all_gene_pairs.txt
+python compute_SSN.py -background="${out}/all_gene_pairs.txt" -pvalue=$pvalue -ref="$normal_gene_matrix" -sample="$case_gene_matrix" -out="$out"
--- SSN/interaction.py
+++ SSN/interaction.py
@@ -7,6 +7,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-i','--input', type=str,help="The gene expression matrix")
+parser.add_argument('-o','--output', type=str,help="The gene pairs")
 
 args = parser.parse_args()
 
@@ -15,7 +16,7 @@ normal_gene=normal.iloc[:,0]
 x1=list(set(normal_gene))
 x3=list(combinations(x1, 2))
 
-filename=open('all_gene_pairs.txt','w')
+filename=open(args.output+'/all_gene_pairs.txt','w')
 for value in range(len(x3)):
 	for i in x3[value]:
 		filename.write("{}  ".format(i))
