6767aa7850dd7dd0672649a487b4d84ee70732de
dongxuemin666 <dongxuemin1995@163.com>
2021-04-16 11:20:56 +0800

    revise

--- scRNA/doublet_detection.sh
+++ scRNA/doublet_detection.sh
@@ -3,4 +3,8 @@
 input=`sed '/^input_7=/!d;s/.*=//' conf_scRNA.txt`
 output=`sed '/^output_7=/!d;s/.*=//' conf_scRNA.txt`
 
+
+
+mv $input/features.tsv.gz $input/genes.tsv.gz
+
 python doublet_detection.py -i $input -o $output
