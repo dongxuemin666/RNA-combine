1ddcd09f17d6384195d0a406b36016c6cdbc292a
dongxuemin666 <dongxuemin1995@163.com>
2021-10-28 23:02:01 +0800

    review_response1

--- scRNA/doublet_detection.sh
+++ scRNA/doublet_detection.sh
@@ -5,6 +5,10 @@ output=`sed '/^output_7=/!d;s/.*=//' conf_scRNA.txt`
 
 
 
+if [ -x "$input/features.tsv.gz"]; then
 mv $input/features.tsv.gz $input/genes.tsv.gz
+fi
+
+
 
 python doublet_detection.py -i $input -o $output
